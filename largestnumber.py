7c21a9b256fb92066056992a31efb1fab5996267
Devieswaran <devi.eswaran98@gmail.com>
2018-03-30 20:11:06 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the number1:"))
+b=int(input("Enter the number2:"))
+c=int(input("Enter the number3:"))
+if(a>b):
+  if(a>c):
+    print("a is greater")
+elif(b>c):
+  print("b is greater")
+else:
+  print("c is greater")
